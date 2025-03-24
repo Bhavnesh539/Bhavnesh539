509ab25d0858dcd812ea645ce3dbe24aa0c0cac2
Bhavnesh539 <bhavneshd36@gmail.com>
2025-03-24 17:22:55 +0530

    Add files via upload

---
+++ VirtualDice.py
@@ -0,0 +1,34 @@
+import random
+
+
+
+print("""
+      V I R T U A L   D I C E  
+    B Y   B H A V N E S H H
+      
+ _______
+|       |
+|  O   O  |
+|       |
+|  O   O  |
+|_______|
+  
+      
+________________________________________________________________________________________________________________________
+   """)
+
+
+
+
+for loop in range (1000):
+    loop= ("To roll the dice press enter")
+    print(input(loop))
+    print(random.randint(1,6))
+    
+   
+
+
+
+
+
+
